4ae2e4b14868cab5cb6ed6cbd7cf07469d1889b7
Prof-Dionatan <dionatan.markiu@escola.pr.gov.br>
2026-08-13 09:56:14 -0300

    Melhorando o script

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const botoes = document.querySelectorAll('button');
 
 botoes.forEach(botao => {
     botao.addEventListener('click',  curtir)
-
+    let curtiu = false
     function curtir() {
         let curtidas = document.querySelector('span')
-        curtidas.textContent++
+       
+        if(curtiu == false){
+            curtiu = true
+            curtidas.textContent++
+            botao.style.backgroundColor = 'pink'
+        }
+
     }
 });
